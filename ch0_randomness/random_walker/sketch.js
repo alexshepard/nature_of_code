5d2b326c97f1ef1c563b01e9fe643a75e878b2da
Alex Shepard <alex@meatfreezer.com>
2024-11-14 20:03:05 -0800

    random walker who walks

--- ch0_randomness/random_walker/sketch.js
+++ ch0_randomness/random_walker/sketch.js
@@ -8,17 +8,35 @@ class Walker {
         stroke(0);
         point(this.x, this.y);
     }
+
+    step() {
+        const choice = floor(random(4));
+
+        if (choice === 0) {
+            this.x++;
+        } else if (choice === 1) {
+            this.x--;
+        } else if (choice === 2) {
+            this.y++;
+        } else {
+            this.y--;
+        }
+    }
 }
 
 let walker;
 
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(640, 240);
 
     walker = new Walker(width / 2, height / 2);
+
+    background(224);
 }
 
 function draw() {
-    background(220);
+    //background(220);
+
+    walker.step();
     walker.show();
 }
