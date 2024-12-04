c2029ae4070133c7866326b1d3c1d3aaa6a8c658
Alex Shepard <alex@meatfreezer.com>
2024-12-03 18:17:10 -0800

    exercise 1.5

---
+++ ch1_vectors/exercise_1_5/sketch.js
@@ -0,0 +1,57 @@
+class Mover {
+    constructor() {
+        this.position = createVector(random(width), random(height));
+        this.velocity = createVector(0, 0);
+        this.acceleration = createVector(-0.001, 0.01);
+        this.diameter = 48;
+        this.radius = this.diameter / 2;
+        this.topSpeed = 10;
+    }
+
+    update() {
+        if (keyIsDown(UP_ARROW)) {
+            this.velocity.add(this.acceleration);
+        } else if (keyIsDown(DOWN_ARROW)) {
+            this.velocity.sub(this.acceleration);
+        }
+        this.position.add(this.velocity);
+        this.velocity.limit(this.topSpeed);
+    }
+
+    show() {
+        stroke(0);
+        fill(175);
+        circle(this.position.x, this.position.y, this.diameter);
+    }
+
+    checkEdges() {
+        if (this.position.x > width + this.radius) {
+            this.position.x = -this.radius;
+        } else if (this.position.x < -this.radius) {
+            this.position.x = width + this.radius;
+        }
+
+        if (this.position.y > height + this.radius) {
+            this.position.y = -this.radius;
+        } else if (this.position.y < -this.radius) {
+            this.position.y = height + this.radius;
+        }
+    }
+}
+
+
+let mover;
+
+function setup() {
+    createCanvas(640, 240);
+
+    mover = new Mover();
+}
+
+function draw() {
+    background(220);
+
+    mover.update();
+    mover.checkEdges();
+    mover.show();
+}
